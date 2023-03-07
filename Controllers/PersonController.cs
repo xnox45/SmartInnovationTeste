ea6cfb4b389830f8facc457082a9778244db4864
xnox45 <xnox.45@hotmail.com>
2023-03-07 02:35:03 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using Infra.DTOs;
+using Infra.Entities;
+using Infra.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartInnovationTeste.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        IPersonService _personService;
+
+        PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpPost("CreatePerson")]
+        public IActionResult Index(PersonDTO person)
+		{
+            _personService.CreatePerson(person);
+
+			return Ok(person);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Infra.ConfigurationInjector;
+using Infra.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<CadastroContext>(c =>
+                c.UseSqlite("DataSource=app.db;Cache=Shared"));
+builder.Services.RegisterService();
+builder.Services.AddAutoMapper(typeof(AutoMapperSetup));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
